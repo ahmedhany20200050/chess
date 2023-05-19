c43ae7c59b57f787a644ea8e9ad1fd500096bb8e
ahmed <ahmedhany20200050@gmail.com>
2023-05-19 15:10:25 +0300

    initial repo

---
+++ chess.py
@@ -0,0 +1,22 @@
+import chess
+
+def evaluate_board(board):
+    # Simple evaluation function that just counts the material difference
+    piece_values = {
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9,
+        chess.KING: 1000
+    }
+    score = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece is not None:
+            value = piece_values[piece.piece_type]
+            if piece.color == chess.WHITE:
+                score += value
+            else:
+                score -= value
+    return score
