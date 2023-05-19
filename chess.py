4dfcca27bfb17d41a0e4fc35fca96d3afae52adc
Basel85 <baselbasel1515@gmail.com>
2023-05-19 15:58:42 +0300

    minimax algorithm done successfully

--- chess.py
+++ chess.py
@@ -21,6 +21,33 @@ def evaluate_board(board):
                 score -= value
     return score
 
+# Minimax algorithm with alpha-beta pruning
+def minimax(board, depth, alpha, beta, maximizing_player):
+    if depth == 0 or board.is_game_over():
+        return evaluate_board(board)
+    if maximizing_player:
+        max_eval = float('-inf')
+        for move in get_legal_moves(board):
+            board.push(chess.Move.from_uci(move))
+            eval = minimax(board, depth-1, alpha, beta, False)
+            board.pop()
+            max_eval = max(max_eval, eval)
+            alpha = max(alpha, eval)
+            if beta <= alpha:
+                break
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for move in get_legal_moves(board):
+            board.push(chess.Move.from_uci(move))
+            eval = minimax(board, depth-1, alpha, beta, True)
+            board.pop()
+            min_eval = min(min_eval, eval)
+            beta = min(beta, eval)
+            if beta <= alpha:
+                break
+        return min_eval
+
 # Generate legal moves
 def get_legal_moves(board):
     legal_moves = []
