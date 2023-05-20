e09960f71859e10452122a351cd09d4d65fcb34f
ahmed <ahmedhany20200050@gmail.com>
2023-05-20 14:28:43 +0300

    fixed bug

--- chess.py
+++ chess.py
@@ -2,6 +2,11 @@ import chess
 import random
 import time
 
+# Initialize the board
+board = chess.Board()
+
+
+# Define evaluation function
 def evaluate_board(board):
     # Simple evaluation function that just counts the material difference
     piece_values = {
@@ -23,6 +28,15 @@ def evaluate_board(board):
                 score -= value
     return score
 
+
+# Generate legal moves
+def get_legal_moves(board):
+    legal_moves = []
+    for move in board.legal_moves:
+        legal_moves.append(str(move))
+    return legal_moves
+
+
 # Minimax algorithm with alpha-beta pruning
 def minimax(board, depth, alpha, beta, maximizing_player):
     if depth == 0 or board.is_game_over():
@@ -31,7 +45,7 @@ def minimax(board, depth, alpha, beta, maximizing_player):
         max_eval = float('-inf')
         for move in get_legal_moves(board):
             board.push(chess.Move.from_uci(move))
-            eval = minimax(board, depth-1, alpha, beta, False)
+            eval = minimax(board, depth - 1, alpha, beta, False)
             board.pop()
             max_eval = max(max_eval, eval)
             alpha = max(alpha, eval)
@@ -42,7 +56,7 @@ def minimax(board, depth, alpha, beta, maximizing_player):
         min_eval = float('inf')
         for move in get_legal_moves(board):
             board.push(chess.Move.from_uci(move))
-            eval = minimax(board, depth-1, alpha, beta, True)
+            eval = minimax(board, depth - 1, alpha, beta, True)
             board.pop()
             min_eval = min(min_eval, eval)
             beta = min(beta, eval)
@@ -50,12 +64,6 @@ def minimax(board, depth, alpha, beta, maximizing_player):
                 break
         return min_eval
 
-# Generate legal moves
-def get_legal_moves(board):
-    legal_moves = []
-    for move in board.legal_moves:
-        legal_moves.append(str(move))
-    return legal_moves
 
 # Choose the best move for AI agent
 def choose_move(board, depth):
@@ -63,7 +71,7 @@ def choose_move(board, depth):
     max_eval = float('-inf')
     for move in get_legal_moves(board):
         board.push(chess.Move.from_uci(move))
-        eval = minimax(board, depth-1, float('-inf'), float('inf'), False)
+        eval = minimax(board, depth - 1, float('-inf'), float('inf'), False)
         board.pop()
         if eval > max_eval:
             max_eval = eval
@@ -71,7 +79,7 @@ def choose_move(board, depth):
     return best_move
 
 
-    # Make a random move for the computer player
+# Make a random move for the computer player
 def make_random_move(board):
     legal_moves = list(board.legal_moves)
     random_move = random.choice(legal_moves)
@@ -83,8 +91,8 @@ while not board.is_game_over():
     if board.turn == chess.WHITE:
         # AI agent's turn
         print("AI agent is thinking...")
-        time.sleep(3) # Delay of 3 seconds
-        move = choose_move(board, 2) # Depth of 2 for minimax search
+        time.sleep(3)  # Delay of 3 seconds
+        move = choose_move(board, 5)  # Depth of 2 for minimax search
         print("AI agent chooses move:", move)
         board.push(chess.Move.from_uci(move))
     else:
@@ -92,7 +100,7 @@ while not board.is_game_over():
         # Computer player's turn
         make_random_move(board)
         print("Computer player chooses move.")
-        
+
     print(board)
 
 print(board.result())
